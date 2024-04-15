cf93b8a0d9a68fe8ff951388dae32d33428fe0c0
unknown <hadeerraouf4@gmail.com>
2024-04-15 15:43:00 +0200

    formate documents

--- src/pages/LoginView.vue
+++ src/pages/LoginView.vue
@@ -1,38 +1,44 @@
 <template>
-    <div>
-    
-             <!----------------Login form-->
-             <div class="login-container" id="login">
-                <div class="top">
-                    <span class="">Don't have an account?<a href="#" @click="register()">Sign Up</a></span>
-                    <header>Login</header>
-                </div>
-            
-                <div class="input-box">
-                    <input type="text" class="input-field" placeholder="Username or Email">
-                    <i class="fa-solid fa-user"></i>
-                </div>
-                <div class="input-box">
-                    <input type="password" class="input-field" placeholder="Password">
-                    <!-- <i class="fa-light fa-user"></i>-->
-                    <i class="fa-solid fa-lock"></i>
-                    <!-- <i class="box bx-lock-alt"></i> -->
-                </div>
-                <div class="input-box">
-                    <input type="submit" class="submit" value="Sign In">
-                   
-                </div>
-                <div class="two-col">
-                    <div class="one">
-                        <input type="checkbox" id="login-check" class="">
-                        <label for="login-check"> Remember Me</label>
-                    </div>
-                    <div class="two">
-                        <label for=""><a href="#" class="">Forgot Password?</a></label>
-                    </div>
-                </div>
-           
-            <!-- <form>
+  <div>
+    <!----------------Login form-->
+    <div class="login-container" id="login">
+      <div class="top">
+        <span class=""
+          >Don't have an account?<a href="#" @click="register()"
+            >Sign Up</a
+          ></span
+        >
+        <header>Login</header>
+      </div>
+
+      <div class="input-box">
+        <input
+          type="text"
+          class="input-field"
+          placeholder="Username or Email"
+        />
+        <i class="fa-solid fa-user"></i>
+      </div>
+      <div class="input-box">
+        <input type="password" class="input-field" placeholder="Password" />
+        <!-- <i class="fa-light fa-user"></i>-->
+        <i class="fa-solid fa-lock"></i>
+        <!-- <i class="box bx-lock-alt"></i> -->
+      </div>
+      <div class="input-box">
+        <input type="submit" class="submit" value="Sign In" />
+      </div>
+      <div class="two-col">
+        <div class="one">
+          <input type="checkbox" id="login-check" class="" />
+          <label for="login-check"> Remember Me</label>
+        </div>
+        <div class="two">
+          <label for=""><a href="#" class="">Forgot Password?</a></label>
+        </div>
+      </div>
+
+      <!-- <form>
                 <div class="form-group">
                     <label>Email</label>
                     <input type="text" class="form-control"/>
@@ -48,15 +54,15 @@
                     </button>
                 </div>
             </form> -->
-        </div>
     </div>
+  </div>
 </template>
 <script>
-export default{
- methods:{
-    register(){
-        this.$emit("register")
-    }
- }
-  }
+export default {
+  methods: {
+    register() {
+      this.$emit("register");
+    },
+  },
+};
 </script>
